5588e5dc86db04b95e27b6a17e8a784a92602dd7
Pedro Silva <pedro.melo@conductor.com.br>
2021-04-05 15:17:34 -0300

    adicao do aws s3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true
+});
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan("dev"));
+app.use("/files", express.static(path.resolve(__dirname, ".." , "tmp", "uploads")));
 
 app.use(routes);
 app.listen(3333);
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,32 @@
 const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
+const Post = require("./models/Post");
 
-routes.post("/posts", multer(multerConfig).single("file") , (request, response) =>{
-    console.log();
+routes.get("/posts", async (request, response) => {
+    const posts = await Post.find();
+    return response.json(posts);
+});
+
+routes.delete("/posts/:id", async (request, response) => {
+    const post = await Post.findById(request.param.id);
+
+    await post.remove();
+
+    return res.send();
+});
+
+routes.post("/posts", multer(multerConfig).single("file") , async (request, response) =>{
+    const { originalname: name, size , key, location: url = ""} = req.file;
+
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url,
+    });
+
+    return response.json(post).status(201);
 });
 
 module.exports = routes;
